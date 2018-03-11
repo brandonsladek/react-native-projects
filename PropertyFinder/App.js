6da93d09ba2f5399ded925b988fc790ff946a363
brandonsladek <brandonsladek@gmail.com>
2018-03-11 16:14:53 -0600

    initial commit of PropertyFinder tutorial repo

---
+++ PropertyFinder/App.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return React.createElement(Text, {syle: styles.description}, "Search for houses to buy!");
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
