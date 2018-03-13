790fd424942f48523c34168a72f18762e2eb0b56
brandonsladek <brandonsladek@gmail.com>
2018-03-12 18:35:35 -0600

    basic nav in place

--- PropertyFinder/App.js
+++ PropertyFinder/App.js
@@ -5,12 +5,26 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS
 } from 'react-native';
+
+export class SearchPage extends Component<{}> {
+  render() {
+    return <Text style={styles.description}>Search for houses to buy!</Text>;
+  }
+}
 
 export default class App extends Component<{}> {
   render() {
-    return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
   }
 }
 
@@ -19,6 +33,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
     textAlign: 'center',
     color: '#656565',
-    marginTop: 65,
+    marginTop: 100
   },
+  container: {
+    flex: 1
+  }
 });
